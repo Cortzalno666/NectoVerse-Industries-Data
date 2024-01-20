5ed070263f492ebf164c5e15695af0230d633dcd
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-01-20 17:25:21 +0600

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,16 +1,18 @@
 -- Made By KHANTELL on roblox, Creation TEAM NectoVerse
+
+-- Loading Library And Window
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 local window = DrRayLibrary:Load("NectoLauncher V0.0.1", "Default")
-
+-- Tab Creation
 local tab = DrRayLibrary.newTab("Launcher", "ImageIdHere")
 tab.newLabel("Credits to NectoVerse: https://discord.gg/aPevmdnnge")
-
+-- Input For fun
 tab.newInput("Fun Feature", "This is just for fun, nothing interesting. ", function(text)
     print("Entered text: " .. text)
 end)
-
+-- Launching button
 tab.newButton("Launch NectoHub", "NectoHub Launcher V0.0.1", function()
-loadstring(game:HttpGet('https://pastebin.com/raw/jYJQDSP2'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/Nectoverse-Launcher/master/NectoHub.lua'))()
       print('NectoHub V0.0.1 Launched')
 end)
 
