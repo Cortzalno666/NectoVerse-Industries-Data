96f4f052c99bd1aed8291a0581b987a5352e1a20
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-01-22 19:45:19 +0600

    Update Bladeball-LowBudget[NVD].lua

--- Scripts Folder/Bladeball-LowBudget[NVD].lua
+++ Scripts Folder/Bladeball-LowBudget[NVD].lua
@@ -15,7 +15,7 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/vFis
       FileName = "No2024only28373", -- nuh uh
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"No2024only2030,"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"No2024only2030","Raw808"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
  
