c73edfc01cf92101e9a162310368696f6f933f8f
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-01-23 18:32:04 +0600

    Update Ninja-Tycoon-NVD.lua

--- Scripts Folder/Ninja-Tycoon-NVD.lua
+++ Scripts Folder/Ninja-Tycoon-NVD.lua
@@ -1,293 +1,23 @@
-if game.CoreGui:FindFirstChild("emptynessHub") then
-    game.CoreGui.emptynessHub:Destroy()
-end
-game:GetService("UserInputService").InputBegan:connect(
-    function(key, gpe)
-        if key.KeyCode == Enum.KeyCode.RightControl then
-            game.CoreGui.emptynessHub.Enabled = not game.CoreGui.emptynessHub.Enabled
-        end
-    end
-)
-
-local Kavo = {}
-
-local tween = game:GetService("TweenService")
-local tweeninfo = TweenInfo.new
-local input = game:GetService("UserInputService")
-local run = game:GetService("RunService")
-
-local Utility = {}
-local Objects = {}
-function Kavo:DraggingEnabled(frame, parent)
-
-	parent = parent or frame
-
-	-- stolen from wally or kiriot, kek
-	local dragging = false
-	local dragInput, mousePos, framePos
-
-	frame.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			mousePos = input.Position
-			framePos = parent.Position
-
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragging = false
-				end
-			end)
-		end
-	end)
-
-	frame.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-
-	input.InputChanged:Connect(function(input)
-		if input == dragInput and dragging then
-			local delta = input.Position - mousePos
-			parent.Position  = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y)
-		end
-	end)
-end
-
-function Utility:TweenObject(obj, properties, duration, ...)
-	tween:Create(obj, tweeninfo(duration, ...), properties):Play()
-end
-
-local h4x = Instance.new("ScreenGui")
-local main = Instance.new("Frame")
-local title = Instance.new("TextLabel")
-local astolfo_corner = Instance.new("ImageLabel")
-local Xclose = Instance.new("TextButton")
-local ImageLabel = Instance.new("ImageLabel")
-local credits = Instance.new("TextLabel")
-local f_to_noclip = Instance.new("TextButton")
-local remove_force_fields = Instance.new("TextButton")
-local remove_walls = Instance.new("TextButton")
-local get_all_jutsus = Instance.new("TextButton")
-local speed_set = Instance.new("TextButton")
-local speed_textbox = Instance.new("TextBox")
-local t_to_teleport = Instance.new("TextButton")
-local teleport_to_player = Instance.new("TextButton")
-local player_textbox = Instance.new("TextBox")
-local TextLabel = Instance.new("TextLabel")
-local fly = Instance.new("TextButton")
-local astolfo_pet = Instance.new("TextButton")
-local credits_2 = Instance.new("TextLabel")
-
---Properties:
-
-Kavo:DraggingEnabled(main)
-
-h4x.Parent = game.CoreGui
-	h4x.Name = "emptynessHub"
-	h4x.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	h4x.ResetOnSpawn = false
-
-main.Name = "main"
-main.Parent = h4x
-main.BackgroundColor3 = Color3.fromRGB(153, 20, 60)
-main.BorderColor3 = Color3.fromRGB(56, 3, 0)
-main.BorderSizePixel = 2
-main.Position = UDim2.new(0.703225791, 0, 0.0702341124, 0)
-main.Size = UDim2.new(0, 281, 0, 498)
-
-title.Name = "title"
-title.Parent = main
-title.BackgroundColor3 = Color3.fromRGB(105, 13, 28)
-title.BorderColor3 = Color3.fromRGB(56, 3, 0)
-title.BorderSizePixel = 4
-title.Size = UDim2.new(0, 281, 0, 52)
-title.Font = Enum.Font.SourceSans
-title.Text = "Ninja Tycoon GUI old"
-title.TextColor3 = Color3.fromRGB(255, 255, 255)
-title.TextSize = 45.000
-
-astolfo_corner.Name = "astolfo_corner"
-astolfo_corner.Parent = main
-astolfo_corner.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-astolfo_corner.BackgroundTransparency = 1.000
-astolfo_corner.Position = UDim2.new(-0.55160141, 0, 0.710843384, 0)
-astolfo_corner.Size = UDim2.new(0, 155, 0, 144)
-astolfo_corner.Image = "rbxassetid://6747647676"
-astolfo_corner.ImageTransparency = 0.100
-
-Xclose.Name = "Xclose"
-Xclose.Parent = main
-Xclose.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Xclose.BackgroundTransparency = 1.000
-Xclose.Position = UDim2.new(0.55160141, 0, 0.927710831, 0)
-Xclose.Size = UDim2.new(0, 126, 0, 36)
-Xclose.Font = Enum.Font.SourceSans
-Xclose.Text = "X"
-Xclose.TextColor3 = Color3.fromRGB(255, 255, 255)
-Xclose.TextScaled = true
-Xclose.TextSize = 45.000
-Xclose.TextWrapped = true
-Xclose.MouseButton1Click:Connect(function()
-h4x:Destroy()
-end)
-
-f_to_noclip.Name = "f_to_noclip"
-f_to_noclip.Parent = main
-f_to_noclip.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-f_to_noclip.BorderSizePixel = 0
-f_to_noclip.Position = UDim2.new(0, 0, 0.126506031, 0)
-f_to_noclip.Size = UDim2.new(0, 98, 0, 40)
-f_to_noclip.Font = Enum.Font.SourceSans
-f_to_noclip.Text = "F to noclip"
-f_to_noclip.TextColor3 = Color3.fromRGB(52, 0, 49)
-f_to_noclip.TextSize = 25.000
-
-remove_force_fields.Name = "remove_force_fields"
-remove_force_fields.Parent = main
-remove_force_fields.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-remove_force_fields.BorderSizePixel = 0
-remove_force_fields.Position = UDim2.new(0.373665482, 0, 0.126506031, 0)
-remove_force_fields.Size = UDim2.new(0, 176, 0, 40)
-remove_force_fields.Font = Enum.Font.SourceSans
-remove_force_fields.Text = "Remove enemy forcefields"
-remove_force_fields.TextColor3 = Color3.fromRGB(52, 0, 49)
-remove_force_fields.TextSize = 19.000
-
-remove_walls.Name = "remove_walls"
-remove_walls.Parent = main
-remove_walls.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-remove_walls.BorderSizePixel = 0
-remove_walls.Position = UDim2.new(0, 0, 0.220883533, 0)
-remove_walls.Size = UDim2.new(0, 98, 0, 40)
-remove_walls.Font = Enum.Font.SourceSans
-remove_walls.Text = "Remove enemy walls"
-remove_walls.TextColor3 = Color3.fromRGB(52, 0, 49)
-remove_walls.TextScaled = true
-remove_walls.TextSize = 19.000
-remove_walls.TextWrapped = true
-
-get_all_jutsus.Name = "get_all_jutsus"
-get_all_jutsus.Parent = main
-get_all_jutsus.BackgroundColor3 = Color3.fromRGB(255, 41, 44)
-get_all_jutsus.BorderSizePixel = 0
-get_all_jutsus.Position = UDim2.new(0.373665482, 0, 0.220883533, 0)
-get_all_jutsus.Size = UDim2.new(0, 176, 0, 40)
-get_all_jutsus.Font = Enum.Font.SourceSans
-get_all_jutsus.Text = "Get all jutsus (OP)"
-get_all_jutsus.TextColor3 = Color3.fromRGB(52, 0, 49)
-get_all_jutsus.TextSize = 25.000
-get_all_jutsus.TextWrapped = true
-
-speed_set.Name = "speed_set"
-speed_set.Parent = main
-speed_set.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-speed_set.BorderSizePixel = 0
-speed_set.Position = UDim2.new(0, 0, 0.313253015, 0)
-speed_set.Size = UDim2.new(0, 98, 0, 40)
-speed_set.Font = Enum.Font.SourceSans
-speed_set.Text = "Set speed"
-speed_set.TextColor3 = Color3.fromRGB(52, 0, 49)
-speed_set.TextSize = 25.000
-
-speed_textbox.Name = "speed_textbox"
-speed_textbox.Parent = main
-speed_textbox.BackgroundColor3 = Color3.fromRGB(250, 89, 255)
-speed_textbox.BorderSizePixel = 0
-speed_textbox.Position = UDim2.new(0.373665482, 0, 0.313253015, 0)
-speed_textbox.Size = UDim2.new(0, 176, 0, 40)
-speed_textbox.ClearTextOnFocus = false
-speed_textbox.Font = Enum.Font.SourceSans
-speed_textbox.Text = "100"
-speed_textbox.TextColor3 = Color3.fromRGB(0, 0, 0)
-speed_textbox.TextScaled = true
-speed_textbox.TextSize = 1.000
-speed_textbox.TextWrapped = true
-
-t_to_teleport.Name = "t_to_teleport"
-t_to_teleport.Parent = main
-t_to_teleport.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-t_to_teleport.BorderSizePixel = 0
-t_to_teleport.Position = UDim2.new(0, 0, 0.405622482, 0)
-t_to_teleport.Size = UDim2.new(0, 281, 0, 35)
-t_to_teleport.Font = Enum.Font.SourceSans
-t_to_teleport.Text = "T to teleport to random player"
-t_to_teleport.TextColor3 = Color3.fromRGB(52, 0, 49)
-t_to_teleport.TextSize = 25.000
-
-teleport_to_player.Name = "teleport_to_player"
-teleport_to_player.Parent = main
-teleport_to_player.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-teleport_to_player.BorderSizePixel = 0
-teleport_to_player.Position = UDim2.new(0, 0, 0.489959836, 0)
-teleport_to_player.Size = UDim2.new(0, 98, 0, 40)
-teleport_to_player.Font = Enum.Font.SourceSans
-teleport_to_player.Text = "Tp to player"
-teleport_to_player.TextColor3 = Color3.fromRGB(52, 0, 49)
-teleport_to_player.TextSize = 25.000
-
-player_textbox.Name = "player_textbox"
-player_textbox.Parent = main
-player_textbox.BackgroundColor3 = Color3.fromRGB(250, 89, 255)
-player_textbox.BorderSizePixel = 0
-player_textbox.Position = UDim2.new(0.373665482, 0, 0.489959836, 0)
-player_textbox.Size = UDim2.new(0, 176, 0, 40)
-player_textbox.ClearTextOnFocus = false
-player_textbox.Font = Enum.Font.SourceSans
-player_textbox.Text = ""
-player_textbox.TextColor3 = Color3.fromRGB(0, 0, 0)
-player_textbox.TextScaled = true
-player_textbox.TextSize = 1.000
-player_textbox.TextWrapped = true
-
-TextLabel.Parent = main
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Position = UDim2.new(0, 0, 0.570281148, 0)
-TextLabel.Size = UDim2.new(0, 281, 0, 50)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "You can type out a small part of someone's username ^^"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 25.000
-TextLabel.TextWrapped = true
-
-fly.Name = "fly"
-fly.Parent = main
-fly.BackgroundColor3 = Color3.fromRGB(219, 38, 104)
-fly.BorderSizePixel = 0
-fly.Position = UDim2.new(0, 0, 0.670682728, 0)
-fly.Size = UDim2.new(0, 281, 0, 35)
-fly.Font = Enum.Font.SourceSans
-fly.Text = "E to Fly"
-fly.TextColor3 = Color3.fromRGB(52, 0, 49)
-fly.TextSize = 25.000
-
-astolfo_pet.Name = "astolfo_pet"
-astolfo_pet.Parent = main
-astolfo_pet.BackgroundColor3 = Color3.fromRGB(219, 33, 52)
-astolfo_pet.BorderSizePixel = 0
-astolfo_pet.Position = UDim2.new(0, 0, 0.753012061, 0)
-astolfo_pet.Size = UDim2.new(0, 281, 0, 35)
-astolfo_pet.Font = Enum.Font.SourceSans
-astolfo_pet.Text = "Astolfo pet!"
-astolfo_pet.TextColor3 = Color3.fromRGB(52, 0, 49)
-astolfo_pet.TextSize = 25.000
-
-credits_2.Name = "credits"
-credits_2.Parent = main
-credits_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-credits_2.BackgroundTransparency = 1.000
-credits_2.Position = UDim2.new(0, 0, 1, 0)
-credits_2.Size = UDim2.new(0, 281, 0, 36)
-credits_2.Font = Enum.Font.SourceSans
-credits_2.Text = "This script is not mind"
-credits_2.TextColor3 = Color3.fromRGB(255, 0, 81)
-credits_2.TextSize = 35.000
-
--- Scripts:
-
-local function WJTYAY_fake_script() -- f_to_noclip.LocalScript 
+-- Loading UI Library | Mercury Library
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+-- Creating the GUI
+local GUI = Mercury:Create{
+    Name = "NectoVerse",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = NectoVerse.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+-- Creating the Tab 1
+local Tab = GUI:Tab{
+	Name = "Starter TAB",
+	Icon = "rbxassetid://8569322835"
+}
+-- buttons
+Tab:Button{
+	Name = "F To noclip",
+	Description = nil,
+	Callback = function()
+	local function WJTYAY_fake_script() -- f_to_noclip.LocalScript 
 	local script = Instance.new('LocalScript', f_to_noclip)
 
 	function do_stuff()
@@ -307,10 +37,17 @@ local function WJTYAY_fake_script() -- f_to_noclip.LocalScript
 			end
 		end)
 	end
-	
-	script.Parent.MouseButton1Click:Connect(do_stuff)
+   script.Parent.MouseButton1Click:Connect(do_stuff)
 end
-coroutine.wrap(WJTYAY_fake_script)()
+	
+	end
+}
+
+Tab:Button{
+	Name = "Remove Enemy Base forcefield",
+	Description = nil,
+	Callback = function()
+	coroutine.wrap(WJTYAY_fake_script)()
 local function QHQBD_fake_script() -- remove_force_fields.LocalScript 
 	local script = Instance.new('LocalScript', remove_force_fields)
 
@@ -330,7 +67,15 @@ local function QHQBD_fake_script() -- remove_force_fields.LocalScript
 	-- FOR WALL REMOVAL
 	
 end
-coroutine.wrap(QHQBD_fake_script)()
+	
+	end
+}
+
+Tab:Button{
+	Name = "Remove Enemy Base Wall",
+	Description = nil,
+	Callback = function()
+	coroutine.wrap(QHQBD_fake_script)()
 local function LHGS_fake_script() -- remove_walls.LocalScript 
 	local script = Instance.new('LocalScript', remove_walls)
 
@@ -347,6 +92,14 @@ local function LHGS_fake_script() -- remove_walls.LocalScript
 	script.Parent.MouseButton1Click:Connect(do_stuff)
 	
 end
+	
+	end
+}
+
+Tab:Button{
+	Name = "Get all jutsu from all enemy base (ENEMY BASE NEEDS TO HAVE ATLEAST 1 JUTSU TO GET THE JUTSU FROM THEM)",
+	Description = nil,
+	Callback = function()
 coroutine.wrap(LHGS_fake_script)()
 local function KLNR_fake_script() -- get_all_jutsus.LocalScript 
 	local script = Instance.new('LocalScript', get_all_jutsus)
@@ -369,19 +122,15 @@ local function KLNR_fake_script() -- get_all_jutsus.LocalScript
 	script.Parent.MouseButton1Click:Connect(do_stuff)
 	
 end
-coroutine.wrap(KLNR_fake_script)()
-local function GGAFFDR_fake_script() -- speed_set.LocalScript 
-	local script = Instance.new('LocalScript', speed_set)
-
-	function do_stuff()
-		local localplayer = game.Players.LocalPlayer
-		local speed = tonumber(script.Parent.Parent.speed_textbox.Text)
-		localplayer.Character.Humanoid.WalkSpeed = speed
+		
 	end
-	
-	script.Parent.MouseButton1Click:Connect(do_stuff)
-end
-coroutine.wrap(GGAFFDR_fake_script)()
+}
+
+Tab:Button{
+	Name = "T to Teleport",
+	Description = nil,
+	Callback = function()
+	coroutine.wrap(GGAFFDR_fake_script)()
 local function KMYD_fake_script() -- t_to_teleport.LocalScript 
 	local script = Instance.new('LocalScript', t_to_teleport)
 
@@ -405,27 +154,15 @@ local function KMYD_fake_script() -- t_to_teleport.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(do_stuff)
 end
-coroutine.wrap(KMYD_fake_script)()
-local function GMNEUKG_fake_script() -- teleport_to_player.LocalScript 
-	local script = Instance.new('LocalScript', teleport_to_player)
-
-	function do_stuff()
-		local localplayer = game.Players.LocalPlayer
-		local player = nil
-		local player_name = script.Parent.Parent.player_textbox.Text
-		for i,v in pairs(game.Players:GetPlayers()) do
-			if v.Name:lower():match(player_name) then
-				player = v
-			end
-		end
-		if player ~= nil then
-			localplayer.Character:moveTo(player.Character.Head.Position)
-		end
-	end
 	
-	script.Parent.MouseButton1Click:Connect(do_stuff)
-end
-coroutine.wrap(GMNEUKG_fake_script)()
+	end
+}
+
+Tab:Button{
+	Name = "Fly",
+	Description = nil,
+	Callback = function()
+	coroutine.wrap(GMNEUKG_fake_script)()
 local function AYQGBW_fake_script() -- fly.LocalScript 
 	local script = Instance.new('LocalScript', fly)
 
@@ -514,7 +251,15 @@ local function AYQGBW_fake_script() -- fly.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(do_stuff)
 end
-coroutine.wrap(AYQGBW_fake_script)()
+	
+	end
+}
+
+Tab:Button{
+	Name = "Astolfo Pet",
+	Description = nil,
+	Callback = function()
+        coroutine.wrap(AYQGBW_fake_script)()
 local function SRQF_fake_script() -- astolfo_pet.LocalScript 
 	local script = Instance.new('LocalScript', astolfo_pet)
 
@@ -647,3 +392,9 @@ local function SRQF_fake_script() -- astolfo_pet.LocalScript
 	script.Parent.MouseButton1Click:Connect(do_stuff)
 end
 coroutine.wrap(SRQF_fake_script)()
+	
+	end
+}
+-- Scripts:
+
+
