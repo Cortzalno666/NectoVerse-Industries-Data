58bd2d4c28cd9ce473646ba044f7b7c4f36fbcac
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-01-31 22:45:12 +0600

    Update NVD-Auto-Chest.lua

--- Scripts Folder/NVD-Auto-Chest.lua
+++ Scripts Folder/NVD-Auto-Chest.lua
@@ -4,15 +4,13 @@ getgenv().Config = {
     ["TimeReset"] = 5, -- No Edit
     ["White Screen"] = false,
     ["Stop The God's Chalice"] = true,
-    ["Stop The Fist of Darkness"] = false,
+    ["Stop The Fist of Darkness"] = true,
     ["HopServer"] = {
         ["Enable"] = true,
         ["BypassServer"] = true,
         ["Region"] = "Singapore"
     },
-    ["Server Discord"] = "https://discord.gg/aPevmdnnge"
+    ["Server Discord"] = "https://discord.gg/mFzWdBUn45"
 }
 repeat wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/blob/master/Scripts%20Folder/Scripts%20folder%20%5BV2%5D/Farm.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/blob/master/Scripts%20Folder/Scripts%20folder%20%5BV2%5D/Farm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhtriettt/Free-Script/main/Chest-Farm.lua"))()
