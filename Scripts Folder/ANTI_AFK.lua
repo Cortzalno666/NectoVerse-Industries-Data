30be18799d3170ab772c125c1b1fdec257262c80
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-04-14 18:02:22 +0600

    Update ANTI_AFK.lua

--- Scripts Folder/ANTI_AFK.lua
+++ Scripts Folder/ANTI_AFK.lua
@@ -1,8 +1,12 @@
-wait(0.5)local ba=Instance.new("ScreenGui")
-local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
-local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
+wait(0.5)
+
+local ba=Instance.new("ScreenGui")
+local ca=Instance.new("TextLabel")
+local da=Instance.new("Frame")
+local _b=Instance.new("TextLabel")
+local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
 ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
-ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
+ca.BackgroundColor3=Color3.new(255, 255, 255)ca.Draggable=true
 ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
 ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Univeral Anti-Afk"ca.TextColor3=Color3.new(0,1,1)
 ca.TextSize=22;da.Parent=ca
