73b0c59f8ccd9544b1b224e7210cbb5883d01c5d
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-05-12 20:28:24 +0600

    Update Aimer_hitbox.lua

--- Scripts Folder/Aimer_hitbox.lua
+++ Scripts Folder/Aimer_hitbox.lua
@@ -1,6 +1,6 @@
 -- This aimer is the hitbox as executing the  main script will automatically execute this aswell the  gui.
 
-_G.HeadSize = 25
+_G.HeadSize = 18
 _G.Disabled = true
  
 game:GetService('RunService').RenderStepped:connect(function()
@@ -9,8 +9,8 @@ for i,v in next, game:GetService('Players'):GetPlayers() do
 if v.Name ~= game:GetService('Players').LocalPlayer.Name then
 pcall(function()
 v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
-v.Character.HumanoidRootPart.Transparency = 0.5
-v.Character.HumanoidRootPart.BrickColor = BrickColor.new("White")
+v.Character.HumanoidRootPart.Transparency = 0.9
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Transparent White")
 v.Character.HumanoidRootPart.Material = "Neon"
 v.Character.HumanoidRootPart.CanCollide = false
 end)
