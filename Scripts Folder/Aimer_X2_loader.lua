990ececbfee26adab75df9fb88284c9bd5c06893
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-05-13 18:22:08 +0600

    Update Aimer_X2_loader.lua

--- Scripts Folder/Aimer_X2_loader.lua
+++ Scripts Folder/Aimer_X2_loader.lua
@@ -6,7 +6,7 @@
 local players = game:GetService("Players")
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Note";
-    Text = "You executed Aimer. please note that hitbox,aimbot is automatically executed. mention: " .. players.LocalPlayer.DisplayName;
+    Text = "You executed Aimer. please note that hitbox is auto activated but X2 have manual Aimbot activation, we dont own the aimbot script sadly. mention: " .. players.LocalPlayer.DisplayName;
     Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
     Duration = 5
 })
