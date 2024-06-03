be7633dace3cad4cbcabf40889ea1ded9ea49333
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-06-03 20:56:36 +0600

    Update NectoLauncher.lua

--- NectoLauncher.lua
+++ NectoLauncher.lua
@@ -1,11 +1,10 @@
--- Made By KHANTELL on roblox, Creation TEAM NectoVerse
+-- Made By NectoVerse Development
 -- Launcher Link: loadstring(game:HttpGet(('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/NectoLauncher.lua'),true))()
--- The Creator username is KHANTELL in roblox
--- The creator username is deleted_tensei_lawden in discord
+-- This hub is not a custom scripts hub,this hub contains 3rd party scripts, there are 5 tabs only in 1 tab have our scripts and other tabs are 3rd party. enjoy.
 
 -- RELEASED IN SCRIPT BLOX!
 
--- V0.0.1
+-- Version: 5.0.0 [Released(Beta×)]
 -- NectoLauncher Status: Online
 -- NectoAPI Status: Online
 -- NectoVerse Status: Online
@@ -13,13 +12,13 @@
 
 -- Loading Library And Window
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
-local window = DrRayLibrary:Load("NectoLauncher V0.0.1", "Default")
+local window = DrRayLibrary:Load("NectoLauncher V5.0.0", "Default")
 -- Tab Creation
 local tab = DrRayLibrary.newTab("Launcher", "ImageIdHere")
-tab.newLabel("Credits to NectoVerse: https://discord.gg/aPevmdnnge")
+tab.newLabel("Made by NectoVerse Development")
 -- Netless
-tab.newLabel("User Netless because some scripts might be unusable. ")
-tab.newButton("Launch Netless", "Launch it", function()
+tab.newLabel("netless is very useful ")
+tab.newButton("Launch Netless", "Launching netless, there are some scripts that cannot be used, this script after executed will makee them useable.", function()
 loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()
       print('Netless Launched')
 end)
@@ -30,7 +29,7 @@ end)
 -- Launching button
 tab.newButton("Launch NectoHub", "NectoHub Launcher V0.0.1", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/NectoHub.lua'))()
-      print('NectoHub V2 Launched')
+      print('NectoHub V5.0.0 Launched')
 end)
 
 tab.newLabel("Status Tab")
