2e8831d6d1247d3949748dfdae4a68551c645887
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-06-11 16:28:26 +0600

    Create Slap battles PT.lua

---
+++ Scripts Folder/Slap battles PT.lua
@@ -0,0 +1,212 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("SBPT")
+
+local KillingCheats = PhantomForcesWindow:NewSection("Main")
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("100 players", function()
+    
+print("HI")
+end)
+
+KillingCheats:CreateButton("Place N.39", function()
+    
+print("HI")
+end)
+
+KillingCheats:CreateButton("No oneshot", function()
+    
+print("HI")
+end)
+
+KillingCheats:CreateButton("Testing place", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Glove customization", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Slap royale queue", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Ice essence", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("M-making", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Slap royale matchmaking", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Slap royale", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("??? m", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("??", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("KS only", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Experimental place", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Soft updating dododo", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+KillingCheats:CreateButton("Brazil", function()
+    game:GetService("TeleportService"):Teleport(7234087065)
+print("HI")
+end)
+
+game:GetService("TeleportService"):Teleport(8550289322)  --  100 players|2
+
+game:GetService("TeleportService"):Teleport(8550291053)  --  Place N.(39)|3
+
+game:GetService("TeleportService"):Teleport(9015014224)  --  No oneshot|4
+
+game:GetService("TeleportService"):Teleport(9020359053)  --  Testing|5
+
+game:GetService("TeleportService"):Teleport(9068206286)  --  Glove Customization|6
+
+game:GetService("TeleportService"):Teleport(9412139218)  --  Slap Royale Queue|7
+
+game:GetService("TeleportService"):Teleport(17290438723)  --  Ice essence|8
+
+game:GetService("TeleportService"):Teleport(9426735655)  --  M-Making|9
+
+game:GetService("TeleportService"):Teleport(9426795465) --  Slap Royale matchmaking|10
+
+game:GetService("TeleportService"):Teleport(9431156611)  --  Slap Royale|11
+
+game:GetService("TeleportService"):Teleport(11359765900) --  ??? m|12
+
+game:GetService("TeleportService"):Teleport(11367956190) --  ??|13
+
+game:GetService("TeleportService"):Teleport(11520107397) --  KS only|14
+
+game:GetService("TeleportService"):Teleport(12432152490) --  Experimental Place|15
+
+game:GetService("TeleportService"):Teleport(12712288037) --  Soft updating dododo|16
+
+game:GetService("TeleportService"):Teleport(12845859004) --  Slap battles A|17
+
+game:GetService("TeleportService"):Teleport(13592060273) --  200 players|18
+
+game:GetService("TeleportService"):Teleport(13833961666) --  The dark realm|19
+
+game:GetService("TeleportService"):Teleport(14422118326) --  The null zone|20
+
+game:GetService("TeleportService"):Teleport(15228348051) --  Tower defence|21
+
+game:GetService("TeleportService"):Teleport(15507333474) --  Christmas Event|22
+
+game:GetService("TeleportService"):Teleport(15880267452) --  The C| 23
+
+game:GetService("TeleportService"):Teleport(16034567693) --  Staff application| 24
+
+game:GetService("TeleportService"):Teleport(16467982597) --  Sr matchmaking testing| 25
+
+game:GetService("TeleportService"):Teleport(11828384869) --? |26
+
+local PhantomForcesWindow = Library:NewWindow("Extras")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Slap%20battles.lua"))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Fly.lua'))))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Teleport_Tool.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/ANTI_AFK.lua'))()
+
+local Http = game:GetService("HttpService")
+local TPS = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
+
+local _place = game.PlaceId
+local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+function ListServers(cursor)
+  local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+  return Http:JSONDecode(Raw)
+end
+
+local Server, Next; repeat
+  local Servers = ListServers(Next)
+  Server = Servers.data[1]
+  Next = Servers.nextPageCursor
+until Server
+
+TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
