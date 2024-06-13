49698da80f4dd4b9a2d483e84643da3bddee8e99
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-06-13 20:29:28 +0600

    Update NectoHub.lua

--- NectoHub.lua
+++ NectoHub.lua
@@ -335,24 +335,7 @@ tab.newButton("R20", "well known script", function()
 end)
 
 tab.newButton("Kill aura", "Aura killer", function()
-    function isSpawned(player)
-   if workspace:FindFirstChild(player.Name) and player.Character:FindFirstChild("HumanoidRootPart") then
-       return true
-   else
-       return false
-   end
-end
-
-while wait() do
-   for i, v in pairs(game.Players:GetPlayers()) do
-       if isSpawned(v) and v ~= game.Players.LocalPlayer and not v.Character.Head:FindFirstChild("UnoReverseCard") then
-           if (v.Character.HumanoidRootPart.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 45 then
-               game:GetService("ReplicatedStorage").b:FireServer(v.Character["Right Arm"])
-               wait(0.1)
-           end
-       end
-   end
-end
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Sb%20kill%20aura'),true))()
     print('Hello!')
 end)
 
