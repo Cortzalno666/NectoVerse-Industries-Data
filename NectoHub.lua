7fc7ead5b4e2cc72231708fcb7f3e6f7d82d22e3
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-06-24 21:01:02 +0600

    Update NectoHub.lua

--- NectoHub.lua
+++ NectoHub.lua
@@ -374,11 +374,11 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/balintTheDevX/TigerX
 end)
 
 tab.newButton("Hydroxide", "Spies", function()
-local owner = "Upbolt"
+local owner = "Hosvile"
 local branch = "revision"
 
 local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/MC-Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
 end
 
 webImport("init")
