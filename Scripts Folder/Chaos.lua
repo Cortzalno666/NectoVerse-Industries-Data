a97fdbf8fb42e63a8b6cc2b2eb1c7407649ce48c
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-07-05 22:54:44 +0600

    Create Chaos.lua

---
+++ Scripts Folder/Chaos.lua
@@ -0,0 +1,15 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("Chaos") 
+
+local KillingCheats = PhantomForcesWindow:NewSection("Main") 
+
+KillingCheats:CreateButton("Free gamepass", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ApollyxnHashed/Chaos-Mobile-Scripts-Kit/main/Chaos%20KIT"))()
+print("HI")
+end)
+
+KillingCheats:CreateButton("Kill aura", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptfree/Free-Scripts/main/chaos", true))()
+print("HI")
+end)
