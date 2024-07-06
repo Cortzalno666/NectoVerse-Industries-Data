6b24e3f9d8b471cb93b597473b7bed509796adea
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-07-06 19:11:27 +0600

    Update Gamepass.giver.lua

--- Scripts Folder/Gamepass.giver.lua
+++ Scripts Folder/Gamepass.giver.lua
@@ -1,4 +1,14 @@
 -- its universal
-if game.CreatorType == Enum.CreatorType.User then game.Players.LocalPlayer.UserId = game.CreatorId end if game.CreatorType == Enum.CreatorType.Group then game.Players.LocalPlayer.UserId = game:GetService("GroupService"):GetGroupInfoAsync(game.CreatorId).Owner.Id end
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow(":) Get yourself")
+
+local KillingCheats = PhantomForcesWindow:NewSection("?")
+
+KillingCheats:CreateButton("Free gamepass", function()
+    if game.CreatorType == Enum.CreatorType.User then game.Players.LocalPlayer.UserId = game.CreatorId end if game.CreatorType == Enum.CreatorType.Group then game.Players.LocalPlayer.UserId = game:GetService("GroupService"):GetGroupInfoAsync(game.CreatorId).Owner.Id end
 
 game.Players.LocalPlayer.Character.Humanoid.Health = 0
+print("HI")
+end)
+
