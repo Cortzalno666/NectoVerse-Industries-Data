bba83c5681abc30ffc8fcf965f3646656d5f8aba
Cortzal <148848858+Cortzalno666@users.noreply.github.com>
2024-07-06 19:15:07 +0600

    Create Executor.lua

---
+++ Scripts Folder/Executor.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("Executor")
+
+local KillingCheats = PhantomForcesWindow:NewSection("?")
+
+
+KillingCheats:CreateTextbox("Paste code", function(text)
+    local function q()
+    tb.Text = "" 
+end
+        print(text)
+end)
+
+KillingCheats:CreateButton("Execute", function()
+    local function n()
+    local success, errorMsg = pcall(function()
+        loadstring(tb.Text)()
+    end)
+print("HI")
+end)
