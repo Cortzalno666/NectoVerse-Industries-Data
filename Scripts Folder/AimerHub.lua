8a2dc23eda9e79b1c15ab77b216462e243b51963
Tensei Lawden <148848858+Cortzalno666@users.noreply.github.com>
2024-10-05 13:21:39 +0600

    Update AimerHub.lua

--- Scripts Folder/AimerHub.lua
+++ Scripts Folder/AimerHub.lua
@@ -1,7 +1,31 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/refs/heads/master/Scripts%20Folder/AimerHub%20%5BStracLib%5D.lua"))()
+library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EngineLibraryX/StracLib/refs/heads/main/StracLib.lua'))()
 
 local Main_ = libary:CreateMain()
 
 Main_:CreateButton("Aimer X1", "Load the first ever aimer!", "", "", "", "", function()
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer_loader.lua'))()
+end)
+
+Main_:CreateButton("Aimer X2", "Load X2", "", "", "", "", function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer_X2_loader.lua"))()
+end)
+
+Main_:CreateButton("Aimer Z1", "Load Z1", "", "", "", "", function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer%20Z1.lol"))()
+end)
+
+Main_:CreateButton("Aimer Z2", "Load X2", "", "", "", "", function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer%20Z2.lol"))()
+end)
+
+Main_:CreateButton("Aimer Z3pe", "Load Z3pe", "", "", "", "", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer%20Z3.lol'))()
+end)
+
+Main_:CreateButton("Aimer Z4s", "Load Z4s", "", "", "", "", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer%20Z4.lol'))()
+end)
+
+Main_:CreateButton("Aimer Z5b", "Load Z5b", "", "", "", "", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer%20Z5.lol'))()
 end)
