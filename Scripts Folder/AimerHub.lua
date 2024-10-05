0b480e33a5a3a0c40a24bdaa46128e2503021bcf
Tensei Lawden <148848858+Cortzalno666@users.noreply.github.com>
2024-10-05 12:19:39 +0600

    Create AimerHub.lua

---
+++ Scripts Folder/AimerHub.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/refs/heads/master/Scripts%20Folder/AimerHub%20%5BStracLib%5D.lua"))()
+
+local Main_ = libary:CreateMain()
+
+Main_:CreateButton("Aimer X1", "Load the first ever aimer!", "", "", "", "", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Cortzalno666/NectoVerse-Industries-Data/master/Scripts%20Folder/Aimer_loader.lua'))()
+end)
