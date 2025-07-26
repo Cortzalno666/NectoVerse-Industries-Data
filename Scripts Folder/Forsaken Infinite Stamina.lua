f748f5ed36d02bcc036dbe4f61066cc77b57e5db
Tensei Lawden <148848858+Cortzalno666@users.noreply.github.com>
2025-07-26 12:26:55 +0600

    Update and rename Inf stam.lua to Forsaken Infinite Stamina.lua

--- Scripts Folder/Forsaken Infinite Stamina.lua
+++ Scripts Folder/Forsaken Infinite Stamina.lua
@@ -4,5 +4,5 @@ stamina.MaxStamina = 100  -- Maximum stamina
 stamina.MinStamina = -20  -- Minimum stamina
 stamina.StaminaGain = 100 -- Stamina gain
 stamina.StaminaLoss = 5 -- Stamina loss
-stamina.SprintSpeed = 25 -- Sprspeed
+stamina.SprintSpeed = 26 -- Regular sprint speed
 stamina.StaminaLossDisabled = true -- Disable stamina drain (true/false)
